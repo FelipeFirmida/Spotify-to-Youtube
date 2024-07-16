2aba15def311db10bc691f72959ce06bb8f21f97
Felipe <felipefirmida@gmail.com>
2024-07-15 21:17:50 -0300

    Start ignoring clientinfo.py

--- clientinfo.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ['SPOTIFY_CLIENT_ID'] = "2f94464e7c384a6e83370bc6f7eba6a9"
-os.environ['SPOTIFY_CLIENT_SECRET'] = "f1a24afa6b524d7ebefb27e1e06822e4"
-os.environ['FLASK_S_KEY'] = "Wh4t_1s_G01ng_0N_1989"
